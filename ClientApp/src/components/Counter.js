284c9ae885689f609185ac90581e3622987b5aac
FahadHameed89 <69335527+FahadHameed89@users.noreply.github.com>
2020-11-10 11:34:17 -0700

    Updated with inclass solution;

--- ClientApp/src/components/Counter.js
+++ ClientApp/src/components/Counter.js
@@ -1,35 +1,25 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-export class Counter extends Component {
-  static displayName = Counter.name;
+function Counter(props) {
+    const [currentCount, setCurrentCount] = useState(0);
+    const displayName = props.name;
 
-  constructor(props) {
-      super(props);
-      // Configure our initial state in the constructor
-      // When the state changes, it triggers a refresh
-      this.state = { currentCount: 0 };
+    function incrementCounter() {
+        setCurrentCount(currentCount + 1);
+    }
 
-      // Bind the incrementCounter() method so that it can be used onclick.
-    this.incrementCounter = this.incrementCounter.bind(this);
-  }
-
-  incrementCounter() {
-    this.setState({
-      currentCount: this.state.currentCount + 1
-    });
-  }
-
-  render() {
     return (
-      <div>
-        <h1>Counter</h1>
+        <div>
+            <h1>Counter</h1>
 
-        <p>This is a simple example of a React component.</p>
+            <p>This is a simple example of a React component.</p>
 
-        <p aria-live="polite">Current count: <strong>{this.state.currentCount}</strong></p>
+            <p aria-live="polite">Current count: <strong>{currentCount}</strong></p>
 
-        <button className="btn btn-primary" onClick={this.incrementCounter}>Increment</button>
-      </div>
+            <button className="btn btn-primary" onClick={incrementCounter}>Increment</button>
+        </div>
     );
-  }
 }
+
+export { Counter };
+
